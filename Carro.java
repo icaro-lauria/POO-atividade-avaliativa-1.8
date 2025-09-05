03ec6b16b4cec3661487be17e28309fe5d3fc2b5
icaro-lauria <icaro.lauria@gmail.com>
2025-09-05 10:27:47 -0300

    Update Carro.java

--- Carro.java
+++ Carro.java
@@ -2,7 +2,7 @@ package com.veiculos;
 
 public class Carro extends Veiculo{
 
-	int qtdPortas;
+	private int qtdPortas;
 	
 	public Carro(String marca, String modelo, int ano, double valorBase, int qtdPortas) {
 		super(marca, modelo, ano, valorBase);
@@ -14,4 +14,7 @@ public class Carro extends Veiculo{
 		return this.valorBase - ano*0.1*valorBase;
 	}
 
+	public int getQtdPortas() {
+		return this.qtdPortas;
+	}
 }
